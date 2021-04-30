36391060dc7cc0d340203a772fae4169ca064676
Zil Norvilis <zilasino27@gmail.com>
2021-04-30 20:39:22 +0200

    Add flag svg files and basic html

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+let data = {
+    'ad': 'Andorra',
+    'ae': 'United Arab Emirates',
+    'af': 'Afghanistan',
+    'ag': 'Antigua and Barbuda',
+    'ai': 'Anguilla (UK)',
+    'al': 'Albania',
+    'am': 'Armenia',
+    'ao': 'Angola',
+    'ar': 'Argentina',
+    'as': 'American Samoa (USA)',
+    'at': 'Austria',
+    'au': 'Australia',
+    'aw': 'Aruba (Netherlands)',
+    'ax': 'Aland Islands (Finland)',
+    'az': 'Azerbaijan'
+}
